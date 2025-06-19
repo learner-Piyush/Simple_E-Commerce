46c9e330a955b359c653ee3c35970398a5f30c70
learner-Piyush <sciencetechoindia@gmail.com>
2025-06-19 10:12:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const products = [
+        {id: 1, name: "Product 1", price: 49.99},
+        {id: 2, name: "Product 2", price: 99.99},
+        {id: 3, name: "Product 3", price: 199.99},
+    ]
+
+    const cart =[]
+
+    const productList = document.getElementById("product-list")
+    const cartItems = document.getElementById("cart-items")
+    const emptyCartMessage = document.getElementById("empty-cart")
+    const cartTotalMessage = document.getElementById("cart-total")
+    const totalPriceDisplay = document.getElementById("total-price")
+    const checkoutButton = document.getElementById("checkout-button")
+
+    products.forEach(product => {
+        const productDiv = document.createElement("div")
+        productDiv.classList.add("product")
+        productDiv.innerHTML = `<span>${product.name} - ₹${product.price.toFixed(2)}</span>
+        <button data-id="${product.id}">
+        <img src="../Image Folder/icons8-add-to-cart-50.png" alt="">
+        Add to Cart
+        </button>`
+        productList.appendChild(productDiv)
+    });
+
+    productList.addEventListener("click", (e) => {
+        if (e.target.tagName === "BUTTON") {
+            const productID = parseInt(e.target.getAttribute("data-id"))
+            const product = products.find((p) => p.id === productID)
+            addToCart(product)
+        }
+    })
+
+    function addToCart(product) {
+        cart.push(product),
+        renderCart()
+    }
+
+    function renderCart() {
+        cartItems.innerText = ""
+
+        let totalPrice = 0
+
+        if (cart.length > 0) {
+            emptyCartMessage.classList.add("hidden")
+            cartTotalMessage.classList.remove("hidden")
+            cart.forEach((item, index) => {
+                totalPrice += item.price
+
+                const cartItem = document.createElement("div")
+                cartItem.classList.add("item")
+                cartItem.innerHTML = `<span>${item.name} - ₹${item.price.toFixed(2)}</span>`
+                cartItems.appendChild(cartItem)
+
+                totalPriceDisplay.textContent = `₹${totalPrice.toFixed(2)}`
+            });
+        } else {
+            emptyCartMessage.classList.remove("hidden")
+            totalPriceDisplay.textContent = `₹0.00`
+        }
+    }
+
+    checkoutButton.addEventListener("click", () => {
+        cart.length = 0
+        alert("Checkout Successfully!")
+        renderCart()
+    })
+})
